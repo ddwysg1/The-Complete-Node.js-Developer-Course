19b9611a325ee9fcbf24e473cea7b1eeaecaf418
ddwysg1 <ddwysg1@gmail.com>
2018-07-25 17:21:23 +0800

    add POST /user and Token

--- Section7/server/server.js
+++ Section7/server/server.js
@@ -21,7 +21,7 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-    }, (e) => {
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
@@ -92,6 +92,19 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     })
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch ((e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(port, () => {
