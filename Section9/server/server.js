ea9e7d1f8801e090c8b73199cf45e2001fac7a38
ddwysg1 <ddwysg1@gmail.com>
2018-07-27 11:07:48 +0800

    Add css for chat page

--- Section9/server/server.js
+++ Section9/server/server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', function(message, callback){
         console.log('Create message', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is the server');
+        callback();
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
